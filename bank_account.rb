9d12b194f0aa1ace781e74b80155ae1d4bc010e8
Kimberley Joe <kimmycjoe@gmail.com>
2017-10-11 23:52:48 +0000

    Throws error when  minimum balance has not been met.

--- bank_account.rb
+++ bank_account.rb
@@ -1,6 +1,11 @@
 class BankAccount
     attr_reader :balance
+    @@minimum_opening_balance = 200
+    
     def initialize(opening_balance, account_holder)
+      if opening_balance < @@minimum_opening_balance
+        raise ArgumentError
+      end
       @balance = opening_balance
     end
     
@@ -15,8 +20,8 @@ class BankAccount
     def transfer(amount, account)
         withdraw(amount)
         account.deposit(amount)   
-        
     end
+    
 end
 
 
